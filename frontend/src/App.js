c345705613911617cd1e2013e6b739e1133fca06
rokas <dailyuseracc1@gmail.com>
2022-04-11 16:52:56 +0300

    finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
+// libraries
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-
+// Components
 import Navigation from "./components/MainPage/Navigation";
 import Props from "../src/components/Props.js";
 import Usestate from "../src/components/Usestate.js";
-import Useeffect from "../src/components/Useeffect.js";
-import Register from "../src/components/Application/Register";
-import Login from "../src/components/Application/Login";
-
-import "./App.css";
 import Profile from "./components/ApiData/Profile";
 import GetApi from "./components/ApiData/GetApi";
+// Style
+import "./App.css";
 
 function App() {
   return (
@@ -22,10 +20,7 @@ function App() {
         <Routes>
           <Route path="/props" element={<Props />} />
           <Route path="/usestate" element={<Usestate />} />
-          <Route path="/useeffect" element={<Useeffect />} />
           <Route path="/userapi" element={<GetApi />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Register />} />
           <Route path="/profile/:login" element={<Profile />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/MainPage/Navigation.js
+++ frontend/src/components/MainPage/Navigation.js
@@ -6,29 +6,19 @@ import "./style/Navigation.css";
 function Navigation() {
   return (
     <nav className="Nav-container">
-      <h1>School works</h1>
-      <div className="Nav-body">
-        <ul className="Nav-links">
-          <li>
-            <Link to="/props">Props</Link>
-          </li>
-          <li>
-            <Link to="/usestate">useState</Link>
-          </li>
-          <li>
-            <Link to="/useeffect">useEffect</Link>
-          </li>
-          <li>
-            <Link to="/userapi">Api</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-        </ul>
-      </div>
+      <h1>React</h1>
+
+      <ul className="Nav-links">
+        <li>
+          <Link to="/props">Props</Link>
+        </li>
+        <li>
+          <Link to="/usestate">useState</Link>
+        </li>
+        <li>
+          <Link to="/userapi">Api</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
